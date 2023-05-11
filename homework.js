7e819275c57f7a1e748bc61c4f95ba8fd4253592
Va Landon Moua <valandonmoua@gmail.com>
2023-05-11 17:29:10 -0500

    Second Commit
    ...

---
+++ homework.js
@@ -0,0 +1,84 @@
+//Names, Species type, and  of Alice in Wonderland Characters
+let guests = [
+    { 
+        firstName: 'Alice',
+        lastName: 'Liddell',
+        teaCup: true,
+        teaPlate: true,
+        species: 'Human' 
+    },
+        
+    { 
+        firstName: 'White',
+        lastName: 'Rabbit',
+        teaCup: true,
+        teaPlate: true,
+        species: 'Rabbit' 
+    },
+        
+    { 
+        firstName: 'Mad',
+        lastName: 'Hatter',
+        teaCup: true,
+        teaPlate: true,
+        species: 'Human' 
+    },
+        
+    {   
+        firstName: 'Queen',
+        lastName: 'of Hearts',
+        teaCup: true,
+        teaPlate: true,
+        species: 'Human' 
+    },
+        
+    { 
+        firstName: 'Cheshire',
+        lastName: 'Cat',
+        teaCup: true,
+        teaPlate: true,
+        species: 'Cat' 
+    },
+    
+    { 
+        firstName: 'March',
+        lastName: 'Hare',
+        teaCup: true,
+        teaPlate: true, 
+        species: 'Rabbit' 
+    },
+        
+    { 
+        firstName: 'Dormouse',
+        lastName: 'Mouse',
+        teaCup: true,
+        teaPlate: true, 
+        species: 'Mouse' 
+    },
+
+    { 
+        firstName: 'Caterpillar',
+        lastName: 'Bug',
+        teaCup: true,
+        teaPlate: true, 
+        species: 'Insect' 
+    },
+
+    { 
+        firstName: 'King',
+        lastName: 'of Hearts',
+        teaCup: true,
+        teaPlate: true, 
+        species: 'Human' 
+    },
+
+    { 
+        firstName: 'Frog',
+        lastName: 'Footman',
+        teaCup: true,
+        teaPlate: true,
+        species: 'Frog' 
+    }
+];
+
+console.log(guests)
