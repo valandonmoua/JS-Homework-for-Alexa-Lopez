1d290cba139e13c6c1bee8e46f32de5589b13b2c
Va Landon Moua <valandonmoua@gmail.com>
2023-05-11 19:11:55 -0500

    Fifth Commit
    ...

--- homework.js
+++ homework.js
@@ -1,84 +1,83 @@
-//Names, Species type, of Alice in Wonderland Characters and if they have a tea cup and tea plate
+//The List of Tea Guests
 let guests = [
+    
     { 
         firstName: 'Alice',
         lastName: 'Liddell',
-        teaCup: true,
-        teaPlate: true,
-        species: 'Human' 
-    },
-        
-    { 
-        firstName: 'White',
-        lastName: 'Rabbit',
-        teaCup: true,
-        teaPlate: true,
-        species: 'Rabbit' 
-    },
-        
-    { 
-        firstName: 'Mad',
-        lastName: 'Hatter',
-        teaCup: true,
-        teaPlate: true,
-        species: 'Human' 
-    },
-        
-    {   
-        firstName: 'Queen',
-        lastName: 'of Hearts',
-        teaCup: true,
-        teaPlate: true,
-        species: 'Human' 
-    },
-        
-    { 
-        firstName: 'Cheshire',
-        lastName: 'Cat',
-        teaCup: true,
-        teaPlate: true,
-        species: 'Cat' 
-    },
-    
-    { 
-        firstName: 'March',
-        lastName: 'Hare',
-        teaCup: true,
-        teaPlate: true, 
-        species: 'Rabbit' 
-    },
-        
-    { 
-        firstName: 'Dormouse',
-        lastName: 'Mouse',
-        teaCup: true,
-        teaPlate: true, 
-        species: 'Mouse' 
+        species: 'Human'
     },
 
     { 
-        firstName: 'Caterpillar',
+        firstName: 'White', 
+        lastName: 'Rabbit', 
+        species: 'Rabbit' 
+    },
+
+    {   firstName: 'Mad',
+        lastName: 'Hatter',
+        species: 'Human' 
+    },
+
+    { 
+        firstName: 'Queen',
+        lastName: 'of Hearts',
+        species: 'Human' 
+    },
+
+    {   firstName: 'Cheshire',
+        lastName: 'Cat',
+        species: 'Cat' 
+    },
+
+    {   firstName: 'March',
+        lastName: 'Hare',
+        species: 'Rabbit' 
+    },
+
+    {   firstName: 'Dormouse',
+        lastName: 'Mouse',
+        species: 'Mouse' },
+
+    {   firstName: 'Caterpillar',
         lastName: 'Bug',
-        teaCup: true,
-        teaPlate: true, 
         species: 'Insect' 
     },
 
-    { 
-        firstName: 'King',
-        lastName: 'of Hearts',
-        teaCup: true,
-        teaPlate: true, 
-        species: 'Human' 
-    },
-
-    { 
-        firstName: 'Frog',
+    {   firstName: 'Frog',
         lastName: 'Footman',
-        teaCup: true,
-        teaPlate: true,
-        species: 'Frog' 
+        species: 'Frog' },
+
+    {   firstName: 'King',
+        lastName: 'of Hearts',
+        species: 'Human' 
     }
 ];
+  
+  const teaPartyTable = {};
+  
+  for (let i = 0; i < guests.length; i++) {
+    let guest = guests[i];
+  
+    smallDessertOptions = ['Chocolate Pudding', 'Vanilla Ice Cream', 'Strawberry Short Cake', 'Lemon Cake', 'Red Velvet Cake', 'Peach Cobbler', 'Macadamia Nut Cookie', 'Blueberry Muffin', 'Tubby Custard'];
+    smallDessert = smallDessertOptions[Math.floor(smallDessertOptions.length * Math.random())];
+  
+    teaGuest = {};
+    teaGuest.firstName = guest.firstName;
+    teaGuest.lastName = guest.lastName;
+    teaGuest.species = guest.species;
+    teaGuest.teaCup = true
+    teaGuest.teaPlate = true;
+    teaGuest.smallDessert = `${smallDessert}`;
+  
+    teaPartyTable[guest.firstName] = teaGuest;
+  }
 
-console.log(guests)
+  for (const guest in teaPartyTable) {
+    console.log(`First Name: ${teaPartyTable[guest].firstName}`);
+    console.log(`Last Name: ${teaPartyTable[guest].lastName}`);
+    console.log(`Species: ${teaPartyTable[guest].species}`);
+    console.log(`Tea Cup: ${teaPartyTable[guest].teaCup}`);
+    console.log(`Tea Plate: ${teaPartyTable[guest].teaPlate}`);
+    console.log(`Small Dessert: ${teaPartyTable[guest].smallDessert}`);
+    console.log();
+  }
